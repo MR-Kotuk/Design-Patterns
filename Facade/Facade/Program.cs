5fc2018bdb57568c26a8df8f82fe766867fe87f2
MR_Kotuk <mrkotuk333@gmail.com>
2024-04-29 15:26:16 +0200

    1.2 Facade

---
+++ Facade/Facade/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Facade
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Facade facade = new Facade(new Volkswagen(), new Porshe());
+
+            facade.TurnOnCars();
+        }
+    }
+
+    public class Facade
+    {
+        protected Volkswagen Volkswagen;
+        protected Porshe Porshe;
+
+        public Facade(Volkswagen volkswagen, Porshe porshe)
+        {
+            Volkswagen = volkswagen;
+            Porshe = porshe;
+        }
+
+        public void TurnOnCars()
+        {
+            Console.WriteLine("Turn on motors: ");
+            Volkswagen.TurnOnMotor();
+            Porshe.TurnOnMotor();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Ready to drive: ");
+            Volkswagen.Drive();
+            Porshe.Drive();
+        }
+    }
+
+    public class Volkswagen
+    {
+        public void TurnOnMotor()
+        {
+            Console.WriteLine("Volkswagen: motor turn on");
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("Volkswagen: drive");
+        }
+    }
+
+    public class Porshe
+    {
+        public void TurnOnMotor()
+        {
+            Console.WriteLine("Porshe: motor turn on");
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("Porshe: drive");
+        }
+    }
+}
