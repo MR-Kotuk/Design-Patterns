39b8d72d4d472ff7612adeaf037ed39c84acc268
MR_Kotuk <mrkotuk333@gmail.com>
2024-05-13 15:11:16 +0200

    1.8 Mediator

---
+++ Mediator/Mediator/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Mediator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            new Client();
+        }
+    }
+
+    public class Client
+    {
+        public Client()
+        {
+            Input();
+        }
+
+        public void Input()
+        {
+            Sender sender = new Sender();
+            TextField textField = new TextField();
+
+            IMediator mediator = new UIMediator(sender, textField);
+
+            sender.SetMediator(mediator);
+            textField.SetMediator(mediator);
+
+            textField.Read();
+            sender.Send();
+        }
+    }
+
+    public interface IMediator
+    {
+        public void Notify(object sender, string type);
+    }
+
+    public class UIElementBase
+    {
+        protected IMediator Mediator;
+
+        public UIElementBase(IMediator mediator = null)
+        {
+            SetMediator(mediator);
+        }
+
+        public void SetMediator(IMediator mediator)
+        {
+            Mediator = mediator;
+        }
+    }
+
+    public class UIMediator : IMediator
+    {
+        private Sender _sender;
+        private TextField _textField;
+
+        private string _message;
+
+        public UIMediator(Sender sender, TextField textField)
+        {
+            _sender = sender;
+            _textField = textField;
+        }
+
+        public void Notify(object sender, string type)
+        {
+            if (type == "Read")
+                _message = _textField.Reading();
+            else if (type == "Send" && _message != null)
+                _sender.Sending(_message);
+        }
+    }
+
+    public class Sender : UIElementBase
+    {
+        public void Send()
+        {
+            Mediator.Notify(this, "Send");
+        }
+
+        public void Sending(string message)
+        {
+            Console.WriteLine("Sending...");
+            Console.WriteLine(message);
+        }
+    }
+
+    public class TextField : UIElementBase
+    {
+        public void Read()
+        {
+            Mediator.Notify(this, "Read");
+        }
+
+        public string Reading()
+        {
+            Console.WriteLine("Write something:");
+            return Console.ReadLine();
+        }
+    }
+}
